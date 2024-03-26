9c9987eb583161b2d4ff5d0aaefc21b3cc196406
phu.ma <phu.ma>
2024-03-26 18:06:27 +0700

    change setting to typeorm

--- mikro-orm.config.js
+++
@@ -1,6 +0,0 @@
-import { MikroORM } from '@mikro-orm/postgresql'; // or any other SQL driver package
-
-const orm = await MikroORM.init({
-  entities: [], // path to your JS entities (dist), relative to `baseDir`
-  dbName: 'product-management',
-});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import config from '../mikro-orm.config';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(config)],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'product_management',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
